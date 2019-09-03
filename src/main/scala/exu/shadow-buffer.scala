49bdfab4369b0f25ae650d1a711fc8a0084f5fe8
Erling Rennemo Jellum <erlingrj@stud.ntnu.no>
2019-09-03 14:47:24 +0200

    Adding some debugging to SB

--- src/main/scala/exu/shadow-buffer.scala
+++ src/main/scala/exu/shadow-buffer.scala
@@ -17,16 +17,21 @@ import chisel3._
 import boom.common._
 import boom.util._
 
+import freechips.rocketchip.config.Parameters
+
+
 class ShadowBufferIo(implicit p: Parameters) extends BoomBundle()(p) {
   // From ROB
-  val rob_enque_uop = Input(Bool())
+  val rob_enq = Input(Bool())
   val rob_commit_uop = Input(UInt(SB_ADDR_SZ.W))
+  val rob_commit_valid = Input(Bool())
 
   //
   val sb_tail = Output(UInt(SB_ADDR_SZ.W))
   val sb_head = Output(UInt(SB_ADDR_SZ.W))
 
   val sb_full = Output(Bool())
+  val sb_empty = Output(Bool())
 }
 
 
@@ -39,8 +44,40 @@ class ShadowBuffer(implicit p: Parameters) extends BoomModule()(p)
   val sb_tail     = RegInit(0.U(SB_ADDR_SZ.W))
   val sb_head     = RegInit(0.U(SB_ADDR_SZ.W))
 
-  // Actual buffer
+  // Actual buffer. Only need 1 bit per entry T/F
+  val sb_data      = Mem(NUM_SB_ENTRIES, Bool())
 
+
+  when (io.rob_enq)
+  {
+    sb_data(sb_tail) := true.B
+    sb_tail := sb_tail + 1.U
+
+    // Wrap around (its a circular buffer
+    when (sb_tail === NUM_SB_ENTRIES.U)
+    {
+      sb_tail := 0.U(SB_ADDR_SZ.W)
+    }
+
+    when (sb_tail === sb_head) {
+      io.sb_full := true.B
+    }
+
+    if (DEBUG_PRINTF) {
+      printf("SB received rob_enq signal. tail=%d\n", sb_tail)
+    }
+  }
+  when (io.rob_commit_valid)
+  {
+    sb_data(io.rob_commit_uop) := false.B
+    if (DEBUG_PRINTF) {
+      printf("SB received commit signal for idx=%d\n", io.rob_commit_uop)
+    }
+
+  }
+
+  io.sb_tail := sb_tail
+  io.sb_head := sb_head
 
 
 
