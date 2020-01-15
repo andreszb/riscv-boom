4346aabf1867444f752ab3cd9401a9354333d59c
David Metz <david.metz14@googlemail.com>
2020-01-15 15:21:43 +0100

    LSC issue queue created and integrated into dispatcher

--- src/main/scala/exu/issue-units/issue-units.scala
+++ src/main/scala/exu/issue-units/issue-units.scala
@@ -54,7 +54,11 @@ class IssueUnits(val numWakeupPorts: Int)(implicit val p: Parameters)
 
   // create the issue units (note: this does not create the fp issue unit)
   for (issueParam <- issueParams.filter(_.iqType != IQT_FP.litValue)) {
-    val issue_unit = Module(new IssueUnitCollapsing(issueParam, numWakeupPorts))
+    val issue_unit: IssueUnit = if(boomParams.loadSliceMode){
+      Module(new IssueUnitSlice(issueParam, numWakeupPorts))
+    } else{
+      Module(new IssueUnitCollapsing(issueParam, numWakeupPorts))
+    }
 
     // name the issue units
     if (issueParam.iqType == IQT_INT.litValue) {
