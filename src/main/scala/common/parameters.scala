69a17f119299309d74dce944d5016b3d76e7f9d5
Erling Rennemo Jellum <erlingrj@stud.ntnu.no>
2020-01-20 10:58:12 +0100

    Made length of A/B-Qs a BOOM parameter

--- src/main/scala/common/parameters.scala
+++ src/main/scala/common/parameters.scala
@@ -87,8 +87,9 @@ case class BoomCoreParams(
   useRVE: Boolean = false,
   useBPWatch: Boolean = false,
   clockGate: Boolean = false,
-  loadSliceMode: Boolean = false
-
+  loadSliceMode: Boolean = false,
+  numAqEntries: Int = 8,
+  numBqEntries: Int = 8
 ) extends freechips.rocketchip.tile.CoreParams
 {
   val haveFSDirty = true
@@ -152,6 +153,8 @@ trait HasBoomCoreParameters extends freechips.rocketchip.tile.HasCoreParameters
   val numRcqEntries = boomParams.numRCQEntries       // number of RoCC commit queue entries. This can be large since it just keeps a pdst
   val numLdqEntries = boomParams.numLdqEntries       // number of LAQ entries
   val numStqEntries = boomParams.numStqEntries       // number of SAQ/SDQ entries
+  val numAqEntries  = boomParams.numAqEntries        // number of A-Q entries
+  val numBqEntries  = boomParams.numBqEntries        // number of Bypass-Q entries
   val maxBrCount    = boomParams.maxBrCount          // number of branches we can speculate simultaneously
   val ftqSz         = boomParams.ftq.nEntries        // number of FTQ entries
   val numFetchBufferEntries = boomParams.numFetchBufferEntries // number of instructions that stored between fetch&decode
@@ -269,6 +272,8 @@ trait HasBoomCoreParameters extends freechips.rocketchip.tile.HasCoreParameters
   val stqAddrSz       = log2Ceil(numStqEntries)
   val lsuAddrSz       = ldqAddrSz max stqAddrSz
   val brTagSz         = log2Ceil(maxBrCount)
+  val aqAddrSz        = log2Ceil(numAqEntries)
+  val bqAddrSz        = log2Ceil(numBqEntries)
 
   require (numIntPhysRegs >= (32 + coreWidth))
   require (numFpPhysRegs >= (32 + coreWidth))
