1180474131ce1af232aaad3fff6f426af0db0902
David Metz <david.metz14@googlemail.com>
2020-01-21 11:03:13 +0100

    LSC: fixed bug where instructions that should not go into the b queue but are marked as b are swallowed

--- src/main/scala/exu/dispatch.scala
+++ src/main/scala/exu/dispatch.scala
@@ -126,7 +126,7 @@ class SliceDispatcher(implicit p: Parameters) extends Dispatcher
     // check if b queue can actually process insn
     val can_use_b_alu = uop.fu_code_is(FUConstants.FU_ALU | FUConstants.FU_MUL | FUConstants.FU_DIV)
     val use_b_queue = (uop.uopc === uopLD) || uop.uopc === uopSTA || (uop.is_lsc_b && can_use_b_alu)
-    val use_a_queue = (uop.uopc =/= uopLD) && !uop.is_lsc_b
+    val use_a_queue = (uop.uopc =/= uopLD) && (!uop.is_lsc_b || !can_use_b_alu)
 
     // enqueue logic
     a_queue.io.enq_uops(w).valid := io.ren_uops(w).fire() && use_a_queue
