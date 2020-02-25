2f4e5846d4f5d0a724517982b77f8ca6c65741fa
Erling Rennemo Jellum <erlingrj@stud.ntnu.no>
2020-02-25 14:46:11 +0100

    [RDT] SyncMem working properly now

--- src/main/scala/lsc/InstructionSliceTable.scala
+++ src/main/scala/lsc/InstructionSliceTable.scala
@@ -20,17 +20,131 @@ class IstIO(implicit p: Parameters) extends BoomBundle
   val check = Vec(coreWidth, new IstCheck)
 }
 
+
 class IstMark(implicit p: Parameters) extends BoomBundle
 {
   val mark = ValidIO(UInt(boomParams.loadSliceCore.get.ibda_tag_sz.W))
 }
+
+
+class InstructionSliceTableSyncMem(entries: Int=128, ways: Int=2)(implicit p: Parameters) extends BoomModule{
+  val io = IO(new IstIO)
+  require(isPow2(entries))
+  require(isPow2(ways))
+
+  val tag_table = Reg(Vec(entries, UInt(boomParams.loadSliceCore.get.ibda_tag_sz.W)))
+  val tag_valids = RegInit(VecInit(Seq.fill(entries)(false.B)))
+  val tag_lru = RegInit(VecInit(Seq.fill(entries/2)(false.B)))
+
+  val lscParams = boomParams.loadSliceCore.get
+
+
+  val check_mem_idx = WireInit(VecInit(Seq.fill(decodeWidth)(VecInit(Seq.fill(ways)(0.U(log2Ceil(entries).W))))))
+  val mark_last = WireInit(VecInit(Seq.fill(decodeWidth)(0.U(lscParams.ibda_tag_sz().W))))
+  val mark_curr = WireInit(VecInit(Seq.fill(decodeWidth)(0.U(lscParams.ibda_tag_sz().W))))
+  val select = WireInit(VecInit(Seq.fill(decodeWidth)(0.U(3.W))))
+
+  val mark_last_tag = Reg(Vec(lscParams.rdtIstMarkWidth, UInt(lscParams.ibda_tag_sz.W)))
+  val mark_last_valid = Reg(Vec(lscParams.rdtIstMarkWidth,Bool()))
+
+
+  require(entries == 128)
+  require(ways == 2)
+  def index(i: UInt): UInt = {
+
+    val indexBits = log2Up(entries/ways)
+    val index = Wire(UInt(indexBits.W))
+    if (lscParams.ibdaTagType == IBDA_TAG_FULL_PC) {
+      // xor the second lowest bit with the highest index bit so compressed insns are spread around
+      index := i(indexBits+2-1, 2) ^ Cat(i(1), 0.U((indexBits-1).W))
+    } else if (lscParams.ibdaTagType == IBDA_TAG_INST_LOB) {
+      index := Cat(i(12), i(13), i(5,2)) ^ Cat(i(1), 0.U((indexBits-1).W))
+      // TODO: Research the entropy in the instruction encoding?
+    } else if (lscParams.ibdaTagType == IBDA_TAG_UOPC_LOB) {
+      index := i(indexBits+2-1,2) ^ Cat(i(1), 0.U((indexBits-1).W))
+    }
+
+    index
+
+  }
+  require(ways == 2, "only one lru bit for now!")
+
+
+  // check
+  for(i <- 0 until coreWidth){
+    select(i) := 1.U
+    val tag = io.check(i).tag.bits
+    val idx = index(tag)
+    when(io.check(i).tag.valid){
+      for(j <- 0 until ways){
+        val tidx = (idx << log2Up(ways)).asUInt() + j.U
+        check_mem_idx(i)(j) := tidx
+      }
+
+      // Check if we marked that tag last CC and can do a bypass
+      for(w <- 0 until lscParams.rdtIstMarkWidth){
+        when(mark_last_valid(w) && mark_last_tag(w) === io.check(i).tag.bits) {
+          select := 2.U
+          mark_last(i) := mark_last_tag(w)
+        }
+      }
+    }
+
+  }
+
+
+  // mark - later so mark lrus get priority
+  for(i <- 0 until lscParams.rdtIstMarkWidth){
+    // Store this mark for next CC to bypass
+    mark_last_tag(i) := io.mark(i).mark.bits
+    mark_last_valid(i) := io.mark(i).mark.valid
+
+    when(io.mark(i).mark.valid){
+      val tag = io.mark(i).mark.bits
+      val idx = index(tag)
+      val is_match = WireInit(false.B)
+      for(j <- 0 until ways){
+        val tidx = (idx << log2Up(ways)).asUInt() + j.U
+        when(tag_valids(tidx) && tag_table(tidx) === tag){
+          tag_lru(idx) := j.B // TODO: fix LRU hack
+          is_match := true.B
+        }
+      }
+      when(!is_match){
+        tag_lru(idx) := !tag_lru(idx)
+        val tidx = (idx << log2Up(ways)).asUInt() + !tag_lru(idx)
+        tag_table(tidx) := tag
+        tag_valids(tidx) := true.B
+      }
+
+      // Can we bypass this mark to check port?
+      for (j <- 0 until decodeWidth) {
+        when (tag === io.check(j).tag.bits && io.check(j).tag.valid) {
+          select := 4.U
+          mark_curr(j) := tag
+        }
+      }
+    }
+  }
+
+  // Route out check result (1 CC after the lookup)
+  for (w <- 0 until decodeWidth) {
+    io.check(w).in_ist  := MuxCase(0.U, Array(  select(w)(0).asBool() -> check_mem_idx(w).map(idx => tag_table(idx) === io.check(w).tag.bits).reduce(_||_),
+                                                select(w)(1).asBool() -> RegNext(mark_last(w) === io.check(w).tag.bits),
+                                                select(w)(2).asBool() -> RegNext(mark_curr(w) === io.check(w).tag.bits))
+    )
+  }
+
+  assert(PopCount(select(0)) === 1.U, "[IST] Several bypass-select signals high")
+}
+
 
 class InstructionSliceTable(entries: Int=128, ways: Int=2)(implicit p: Parameters) extends BoomModule{
   val io = IO(new IstIO)
   require(isPow2(entries))
   require(isPow2(ways))
 
-  val tag_table = Reg(Vec(entries, UInt(boomParams.loadSliceCore.get.ibda_tag_sz.W)))
+  val tag_table = SyncReadMem(entries, UInt(boomParams.loadSliceCore.get.ibda_tag_sz.W))
   val tag_valids = RegInit(VecInit(Seq.fill(entries)(false.B)))
   val tag_lru = RegInit(VecInit(Seq.fill(entries/2)(false.B)))
 
--- src/main/scala/lsc/RegisterDependencyTable.scala
+++ src/main/scala/lsc/RegisterDependencyTable.scala
@@ -32,6 +32,14 @@ class RdtUpdateSignals(implicit p: Parameters) extends BoomBundle
   val uop = new MicroOp
 }
 
+class RdtSyncMem(implicit p: Parameters) extends RegisterDependencyTable {
+
+  val rdt = SyncReadMem(boomParams.numIntPhysRegisters, UInt(lscParams.ibda_tag_sz.W))
+  val in_ist = RegInit(VecInit(Seq.fill(boomParams.numIntPhysRegisters)(false.B)))
+
+}
+
+
 class RdtOneBit(implicit p: Parameters) extends RegisterDependencyTable {
 
   val rdt = SyncReadMem(boomParams.numIntPhysRegisters, UInt(lscParams.ibda_tag_sz.W))
@@ -42,18 +50,38 @@ class RdtOneBit(implicit p: Parameters) extends RegisterDependencyTable {
   //  It is probably very common to have such dependency chains and we therefore store the previous update
   //  for bypass
 
+
   val update_dst_l = Reg(Vec(decodeWidth, UInt(log2Ceil(boomParams.numIntPhysRegisters).W)))
   val update_tag_l = Reg(Vec(decodeWidth, UInt(lscParams.ibda_tag_sz().W)))
   val update_in_ist_l = Reg(Vec(decodeWidth, Bool()))
-
-
-  io.mark := DontCare
-  io.mark.map(_.mark.valid := false.B)
 
   // To track which ports have already been used to mark and when we are full
   val mark_port_idx = Wire(Vec(decodeWidth * 2, UInt(lscParams.rdtIstMarkSz.W)))
   val mark_port_used = WireInit(VecInit(Seq.fill(decodeWidth*2)(false.B)))
   val mark_port_full = Wire(Vec(decodeWidth * 2, Bool()))
+
+  // Output signals
+  val mark_valid = WireInit(VecInit(Seq.fill(lscParams.rdtIstMarkWidth)(false.B)))
+  val mark_tag_mem_idx = Wire(Vec(lscParams.rdtIstMarkWidth,  UInt(log2Ceil(boomParams.numIntPhysRegisters).W)))
+  val mark_tag_bypass_last = WireInit(VecInit(Seq.fill(lscParams.rdtIstMarkWidth)(0.U(lscParams.ibda_tag_sz().W))))
+  val mark_tag_bypass_curr = WireInit(VecInit(Seq.fill(lscParams.rdtIstMarkWidth)(0.U(lscParams.ibda_tag_sz().W))))
+  val mark_tag_select = WireInit(VecInit(Seq.fill(lscParams.rdtIstMarkWidth)(1.U(3.W))))
+
+  dontTouch(mark_tag_mem_idx)
+  dontTouch(mark_tag_bypass_last)
+  dontTouch(mark_tag_bypass_curr)
+  dontTouch(mark_tag_select)
+
+  mark_tag_mem_idx.map(_ := 0.U) //Default map to address 0
+
+
+  for (w <- 0 until lscParams.rdtIstMarkWidth) {
+    io.mark(w).mark.valid := RegNext(mark_valid(w))
+    io.mark(w).mark.bits  := MuxCase(mark_tag_mem_idx(w), Array( mark_tag_select(w)(0).asBool() -> rdt(mark_tag_mem_idx(w)),
+                                                            mark_tag_select(w)(1).asBool() -> RegNext(mark_tag_bypass_last(w)),
+                                                            mark_tag_select(w)(2).asBool() -> RegNext(mark_tag_bypass_curr(w)))
+                                                           )
+  }
 
   for (i <- 0 until decodeWidth) {
     val uop = io.update(i).uop
@@ -62,9 +90,9 @@ class RdtOneBit(implicit p: Parameters) extends RegisterDependencyTable {
 
     val is_b = uop.is_lsc_b || uop.uopc === uopLD || uop.uopc === uopSTA || uop.uopc === uopSTD
 
-    // First the logic for getting which mark_port to use and wether it is already full.
+    // First the logic for getting which mark_port to use and whether it is already full.
     //  The logic is based on a SW for-loop. mark_port_idx of current iteration depends on the last iteration
-    //  mark_port_full of this iteration depends on wether last iteration were already full or if it used the port
+    //  mark_port_full of this iteration depends on whether last iteration were already full or if it used the port
     //  and we now are full. It is a little messy since we are looping through 2 ports per iteration (1 per source register)
 
     if (i == 0) { // Special case of first dispatched instr. At this point we dont depend on any previous iteration
@@ -102,16 +130,18 @@ class RdtOneBit(implicit p: Parameters) extends RegisterDependencyTable {
       val port_full = mark_port_full(2 * i)
       when(uop.lrs1_rtype === RT_FIX && uop.prs1 =/= 0.U) {
         when(!port_full) {
-          io.mark(port_idx).mark.valid := RegNext(!in_ist(uop.prs1)) // Only valid when RS1 is not already in IST
+          mark_valid(port_idx) := !in_ist(uop.prs1) // Only valid when RS1 is not already in IST
           port_used := !in_ist(uop.prs1)
-          io.mark(port_idx).mark.bits := rdt(uop.prs1)
+          mark_tag_mem_idx(port_idx) := uop.prs1
+          mark_tag_select(port_idx) := 1.U
 
-          // Bpass RDT for instructions writing to that location in last cycle
+          // Bypass RDT for instructions writing to that location in last cycle
           for (j <- 0 until decodeWidth) {
             when(update_dst_l(j) === uop.prs1) {
-              io.mark(port_idx).mark.valid := RegNext(!update_in_ist_l(j)) // Only valid when RS1 is not already in IST
+              mark_valid(port_idx) := !update_in_ist_l(j) // Only valid when RS1 is not already in IST
               port_used := !update_in_ist_l(j)
-              io.mark(port_idx).mark.bits := RegNext(update_tag_l(j))
+              mark_tag_bypass_last(port_idx) := update_tag_l(j)
+              mark_tag_select(port_idx) := 2.U
             }
           }
 
@@ -120,9 +150,10 @@ class RdtOneBit(implicit p: Parameters) extends RegisterDependencyTable {
             val uop_j = io.update(j).uop
             val uop_j_in_ist = (uop_j.is_lsc_b || uop_j.uopc === uopLD || uop_j.uopc === uopSTA || uop_j.uopc === uopSTD)
             when(commit_dst_valid(j) && uop_j.pdst === uop.prs1) {
-              io.mark(port_idx).mark.valid := RegNext(!uop_j_in_ist)
+              mark_valid(port_idx) := !uop_j_in_ist
               port_used := !uop_j_in_ist
-              io.mark(port_idx).mark.bits := RegNext(io.update(j).tag)
+              mark_tag_bypass_curr(port_idx) := io.update(j).tag
+              mark_tag_select(port_idx) := 4.U
             }
           }
           when(port_used) {
@@ -130,6 +161,7 @@ class RdtOneBit(implicit p: Parameters) extends RegisterDependencyTable {
           }
         }
       }
+
 
       // Mark source register 2
       when(uop.lrs2_rtype === RT_FIX &&
@@ -139,26 +171,27 @@ class RdtOneBit(implicit p: Parameters) extends RegisterDependencyTable {
         val port_used = mark_port_used(2 * i + 1)
         val port_full = mark_port_full(2 * i + 1)
         when(!port_full) {
-          io.mark(port_idx).mark.valid := RegNext(!in_ist(uop.prs2)) // Only valid when RS1 is not already in IST
-          io.mark(port_idx).mark.bits := rdt(uop.prs2)
+          mark_valid(port_idx) := !in_ist(uop.prs2)
+          mark_tag_mem_idx(port_idx) := uop.prs2
           port_used := !in_ist(uop.prs2)
-
-          // Bpass RDT for instructions writing to that location in last cycle
+          mark_tag_select(port_idx) := 1.U
+          // Bypass RDT for instructions writing to that location in last cycle
           for (j <- 0 until decodeWidth) {
             when(update_dst_l(j) === uop.prs2) {
-              io.mark(port_idx).mark.valid := RegNext(!update_in_ist_l(j)) // Only valid when RS1 is not already in IST
+              mark_valid(port_idx) := !update_in_ist_l(j)
               port_used := !update_in_ist_l(j)
-              io.mark(port_idx).mark.bits := RegNext(update_tag_l(j))
+              mark_tag_bypass_last(port_idx) := update_tag_l(j)
+              mark_tag_select(port_idx) := 2.U
             }
-
             // bypass rdt for previous insns written in same cycle
             for (j <- 0 until i) {
               val uop_j = io.update(j).uop
               val uop_j_in_ist = (uop_j.is_lsc_b || uop_j.uopc === uopLD || uop_j.uopc === uopSTA || uop_j.uopc === uopSTD)
               when(commit_dst_valid(j) && uop_j.pdst === uop.prs2) {
-                io.mark(port_idx).mark.valid := RegNext(!uop_j_in_ist)
+                mark_valid(port_idx) := !uop_j_in_ist
                 port_used := !uop_j_in_ist
-                io.mark(port_idx).mark.bits := RegNext(io.update(j).tag)
+                mark_tag_bypass_curr(port_idx) := io.update(j).tag
+                mark_tag_select(port_idx) := 4.U
               }
             }
             when(port_used) {
@@ -171,6 +204,8 @@ class RdtOneBit(implicit p: Parameters) extends RegisterDependencyTable {
   }
 
   assert(PopCount(mark_port_used) <= lscParams.rdtIstMarkWidth.U, "[RDT] try to mark more regs than mark-ports")
+  assert(PopCount(RegNext(mark_port_used)) === PopCount(io.mark.map(_.mark.valid)), "[RDT] used_ports doesnt match valid")
+  assert(PopCount(mark_tag_select(0)) === 1.U, "[RDT] mark_tag_select several hot")
 }
 
 
