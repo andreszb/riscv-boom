8a98115ab974a7a56ca48d043bec585a70b4a5ff
David Metz <david.metz14@googlemail.com>
2020-05-04 16:45:20 +0200

    make rdt use sram

--- src/main/scala/lsc/RegisterDependencyTable.scala
+++ src/main/scala/lsc/RegisterDependencyTable.scala
@@ -5,11 +5,13 @@ import chisel3._
 import chisel3.util._
 import freechips.rocketchip.config.Parameters
 import boom.common._
+import chisel3.iotesters.PeekPokeTester
 import freechips.rocketchip.util.{DescribedSRAM, PopCountAtLeast}
 
 /**
   * Custom commit signals combining ROBs commit signals
   * with a tag that could arrive from somewhere else (like the FTQ)
+  *
   * @param
   */
 
@@ -19,27 +21,130 @@ abstract class RegisterDependencyTable(implicit p: Parameters) extends BoomModul
 }
 
 
-class RdtIO(implicit p: Parameters) extends BoomBundle
-{
+class RdtIO(implicit p: Parameters) extends BoomBundle {
   val update = Input(Vec(decodeWidth, new RdtUpdateSignals))
   val mark = Output(Vec(boomParams.ibdaParams.get.rdtIstMarkWidth, new IstMark))
 }
 
-class RdtUpdateSignals(implicit p: Parameters) extends BoomBundle
-{
+class RdtUpdateSignals(implicit p: Parameters) extends BoomBundle {
   val valid = Bool()
-  val tag  = UInt(boomParams.ibdaParams.get.ibda_tag_sz.W)
+  val tag = UInt(boomParams.ibdaParams.get.ibda_tag_sz.W)
   val uop = new MicroOp
+}
+
+class MultiWriteSram(size: Int, width: Int, reads: Int, writes: Int) extends Module {
+  val io = IO(new Bundle() {
+    val write = Input(Vec(writes, new Bundle {
+      val addr = UInt(log2Floor(size).W)
+      val data = UInt(width.W)
+      val en = Bool()
+    }))
+    val read = Vec(reads, new Bundle() {
+      val addr = Input(UInt(log2Floor(size).W))
+      val data = Output(UInt(width.W))
+    })
+  })
+  // this will be turned to bits because it needs multiple writes - sync becaue it is read in the same cycle as the srams
+  val ways = SyncReadMem(size, UInt(log2Down(writes).W))
+
+  val srams = io.write.map(_ => SyncReadMem(size, UInt(width.W)))
+  val test = SyncReadMem(size, UInt(width.W))
+  val test_valid = RegInit(VecInit(Seq.fill(size)(false.B)))
+  for ((w, i) <- io.write zipWithIndex) {
+    when(w.en) {
+      srams(i).write(w.addr, w.data)
+      // TODO: check colliding writes
+      ways(w.addr) := i.U
+      test(w.addr) := w.data
+      test_valid(w.addr) := true.B
+    }
+  }
+
+  io.read.foreach(r => {
+    val way = WireInit(ways.read(r.addr))
+    way.suggestName("way")
+    dontTouch(way)
+    val reads = VecInit(srams.map(s => s.read(r.addr)))
+    reads.suggestName("reads")
+    dontTouch(reads)
+    val read_data = WireInit(reads(way))
+    read_data.suggestName("read_data")
+    dontTouch(read_data)
+    r.data := read_data
+    val read_valid = WireInit(test_valid(RegNext(r.addr)))
+    read_valid.suggestName("read_valid")
+    dontTouch(read_valid)
+    val test_read = WireInit(test(r.addr))
+    test_read.suggestName("test_read")
+    dontTouch(test_read)
+    assert(!read_valid || (test_read === read_data), "failure in MultiWriteSram")
+  })
+}
+
+object MultiWriteSramTester extends App {
+  val writes = 2
+  val size = 16
+  chisel3.iotesters.Driver.execute(
+    Array(
+      "--backend-name","verilator",
+      "--generate-vcd-output","on",
+    ),
+    () => new MultiWriteSram(size, 32, reads = 1, writes = writes)
+  ) {
+    (c) =>
+      new PeekPokeTester(c) {
+        val r = scala.util.Random
+        c.io.write.foreach(w => {
+          poke(w.en, false.B)
+        })
+        // read empty
+        for(i <- 0 until 16 by 1){
+          poke(c.io.read(0).addr, i)
+          step(1)
+        }
+        // write to different addr
+        for(i <- 0 until 16 by 2){
+          for(j <- 0 until writes){
+            poke(c.io.write(j).addr, i+j)
+            poke(c.io.write(j).data, i+j)
+            poke(c.io.write(j).en, true)
+          }
+          //read at the same time
+          poke(c.io.read(0).addr, i)
+          step(1)
+          c.io.write.foreach(w => {
+            poke(w.en, false.B)
+          })
+          for(j <- 0 until writes){
+            poke(c.io.read(0).addr, i+j)
+            step(1)
+            expect(c.io.read(0).data, i+j)
+          }
+        }
+        // write to same addr
+        for(i <- 0 until 16 by 2){
+          for(j <- 0 until writes){
+            poke(c.io.write(j).addr, i)
+            poke(c.io.write(j).data, i+j)
+            poke(c.io.write(j).en, true)
+          }
+          step(1)
+          c.io.write.foreach(w => {
+            poke(w.en, false.B)
+          })
+          poke(c.io.read(0).addr, i)
+          step(1)
+          // highest write port wins
+          expect(c.io.read(0).data, i+writes-1)
+        }
+        step(1)
+      }
+  }
 }
 
 class RdtSyncMem(implicit p: Parameters) extends RegisterDependencyTable {
 
-  val (rdt, _) = DescribedSRAM(
-    name = s"rdt_ram",
-    desc = "RDT Data Array",
-    size = boomParams.numIntPhysRegisters,
-    data = UInt(ibdaParams.ibda_tag_sz.W)
-  )
+  val sram = Module(new MultiWriteSram(boomParams.numIntPhysRegisters, ibdaParams.ibda_tag_sz, ibdaParams.rdtIstMarkWidth, decodeWidth))
   val in_ist = RegInit(VecInit(Seq.fill(boomParams.numIntPhysRegisters)(false.B)))
   val commit_dst_valid = WireInit(VecInit(Seq.fill(decodeWidth)(false.B)))
 
@@ -48,20 +153,20 @@ class RdtSyncMem(implicit p: Parameters) extends RegisterDependencyTable {
   val rdt1_sram_read_addr = WireInit(VecInit(Seq.fill(ibdaParams.rdtIstMarkWidth)(0.U(log2Ceil(boomParams.numIntPhysRegisters).W))))
 
   // Stage 2
-  val rdt2_mark_tag       = Wire(Vec(ibdaParams.rdtIstMarkWidth, UInt(ibdaParams.ibda_tag_sz.W)))
-  val rdt2_mark_valid     = Wire(Vec(ibdaParams.rdtIstMarkWidth, Bool()))
-  val rdt2_sram_tag       = Wire(Vec(ibdaParams.rdtIstMarkWidth, UInt(ibdaParams.ibda_tag_sz.W)))
-  val rdt2_sram_in_ist    = Reg(Vec(ibdaParams.rdtIstMarkWidth, Bool()))
-  val rdt2_bypass_tag     = Reg(Vec(decodeWidth, UInt(ibdaParams.ibda_tag_sz.W)))
-  val rdt2_bypass_in_ist  = Reg(Vec(decodeWidth, Bool()))
-  val rdt2_bypass_valid   = Reg(Vec(decodeWidth, Bool()))
-  val rdt2_bypass_select  = Reg(Vec(ibdaParams.rdtIstMarkWidth, Bool()))
+  val rdt2_mark_tag = Wire(Vec(ibdaParams.rdtIstMarkWidth, UInt(ibdaParams.ibda_tag_sz.W)))
+  val rdt2_mark_valid = Wire(Vec(ibdaParams.rdtIstMarkWidth, Bool()))
+  val rdt2_sram_tag = Wire(Vec(ibdaParams.rdtIstMarkWidth, UInt(ibdaParams.ibda_tag_sz.W)))
+  val rdt2_sram_in_ist = Reg(Vec(ibdaParams.rdtIstMarkWidth, Bool()))
+  val rdt2_bypass_tag = Reg(Vec(decodeWidth, UInt(ibdaParams.ibda_tag_sz.W)))
+  val rdt2_bypass_in_ist = Reg(Vec(decodeWidth, Bool()))
+  val rdt2_bypass_valid = Reg(Vec(decodeWidth, Bool()))
+  val rdt2_bypass_select = Reg(Vec(ibdaParams.rdtIstMarkWidth, Bool()))
 
 
   // Mark port arbiting
   // To track which ports have already been used to mark and when we are full
   val mark_port_idx = Wire(Vec(decodeWidth * 2, UInt(ibdaParams.rdtIstMarkSz.W)))
-  val mark_port_used = WireInit(VecInit(Seq.fill(decodeWidth*2)(false.B)))
+  val mark_port_used = WireInit(VecInit(Seq.fill(decodeWidth * 2)(false.B)))
   val mark_port_full = Wire(Vec(decodeWidth * 2, Bool()))
 
   // Stage 1
@@ -73,7 +178,8 @@ class RdtSyncMem(implicit p: Parameters) extends RegisterDependencyTable {
 
   for (i <- 0 until ibdaParams.rdtIstMarkWidth) {
     // SRAM read
-    rdt2_sram_tag(i) := rdt(rdt1_sram_read_addr(i))
+    sram.io.read(i).addr := (rdt1_sram_read_addr(i))
+    rdt2_sram_tag(i) := sram.io.read(i).data
     rdt2_sram_in_ist(i) := in_ist(rdt1_sram_read_addr(i))
 
     // IO
@@ -88,8 +194,8 @@ class RdtSyncMem(implicit p: Parameters) extends RegisterDependencyTable {
 
   }
 
-   //Calculate the Read addresses
-   for (i <- 0 until decodeWidth) {
+  //Calculate the Read addresses
+  for (i <- 0 until decodeWidth) {
     val uop = io.update(i).uop
     val valid = io.update(i).valid
     val tag = io.update(i).tag
@@ -119,9 +225,14 @@ class RdtSyncMem(implicit p: Parameters) extends RegisterDependencyTable {
       true.B, mark_port_full(2 * i))
 
 
+    sram.io.write(i).en := false.B
+    sram.io.write(i).addr := DontCare
+    sram.io.write(i).data := DontCare
     // record pc of last insn that writes to reg
     when(valid && uop.dst_rtype === RT_FIX && uop.pdst =/= 0.U) {
-      rdt(uop.pdst) := tag
+      sram.io.write(i).en := true.B
+      sram.io.write(i).addr := uop.pdst
+      sram.io.write(i).data := tag
       in_ist(uop.pdst) := is_b
       commit_dst_valid(i) := true.B
     }
