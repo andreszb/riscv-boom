1305d9f2e0b6b00cf3e33b1a96d9679d3ea00c31
Amund Bergland Kvalsvik <amundbk@stud.ntnu.no>
2020-08-09 15:55:16 +0200

    shadowBuffer now uses coreWidth

--- src/main/scala/exu/shadow-buffer/ShadowBuffer.scala
+++ src/main/scala/exu/shadow-buffer/ShadowBuffer.scala
@@ -8,10 +8,10 @@ import chisel3._
 class ShadowBuffer(implicit p: Parameters) extends BoomModule {
 
   val io = new Bundle {
-    val new_speculative_in = Input(Bool())
+    val new_speculative_in = Input(Vec(coreWidth, Bool()))
 
-    val no_longer_speculative_index_in = Input(UInt(8.W))
-    val no_longer_speculative_committed = Input(Bool())
+    val no_longer_speculative_index_in = Input(Vec(coreWidth, UInt(8.W)))
+    val no_longer_speculative_committed = Input(Vec(coreWidth, Bool()))
 
     val shadow_buffer_head_out = Output(UInt(8.W))
     val shadow_buffer_tail_out = Output(UInt(8.W))
@@ -26,16 +26,19 @@ class ShadowBuffer(implicit p: Parameters) extends BoomModule {
   io.shadow_buffer_head_out := ShadowBufferHead
   io.shadow_buffer_tail_out := ShadowBufferTail
 
-  when(io.new_speculative_in) {
-    ShadowBufferTail := (ShadowBufferTail + 1.U) % 64.U
-    ShadowCaster(ShadowBufferTail) := true.B
+  for (w <- 0 until coreWidth) {
+    when(io.new_speculative_in(w)) {
+      ShadowBufferTail := (ShadowBufferTail + 1.U) % 64.U
+      ShadowCaster(ShadowBufferTail) := true.B
+    }
+
+    when(io.no_longer_speculative_committed(w)) {
+      ShadowCaster(io.no_longer_speculative_index_in(w)) := false.B
+    }
+
+    when(ShadowCaster(ShadowBufferHead) === false.B) {
+      ShadowBufferHead := (ShadowBufferHead + 1.U) % 64.U
+    }
   }
 
-  when(io.no_longer_speculative_committed) {
-    ShadowCaster(io.no_longer_speculative_index_in) := false.B
-  }
-
-  when(ShadowCaster(ShadowBufferHead) === false.B) {
-    ShadowBufferHead := (ShadowBufferHead + 1.U) % 64.U
-  }
 }
