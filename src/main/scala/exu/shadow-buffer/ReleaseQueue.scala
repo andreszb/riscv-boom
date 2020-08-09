8357eafbb9172704a85172460c45c6b366d6c5de
Amund Bergland Kvalsvik <amundbk@stud.ntnu.no>
2020-08-09 15:50:04 +0200

    releaseQueue uses coreWidth now

--- src/main/scala/exu/shadow-buffer/ReleaseQueue.scala
+++ src/main/scala/exu/shadow-buffer/ReleaseQueue.scala
@@ -8,14 +8,14 @@ import chisel3._
 class ReleaseQueue(implicit p: Parameters) extends BoomModule {
 
   val io = new Bundle {
-    val new_entry_in = Input(Bool())
+    val new_entry_in = Input(Vec(coreWidth, Bool()))
+    val load_queue_index_in = Input(Vec(coreWidth, UInt()))
 
     val shadow_buffer_head_in = Input(UInt())
     val shadow_buffer_tail_in = Input(UInt())
-    val load_queue_index_in = Input(UInt())
 
-    val load_queue_index_out = Output(UInt())
-    val should_release_load_out = Output(Bool())
+    val load_queue_index_out = Output(Vec(coreWidth, UInt()))
+    val should_release_load_out = Output(Vec(coreWidth, Bool()))
 
   }
 
@@ -26,18 +26,21 @@ class ReleaseQueue(implicit p: Parameters) extends BoomModule {
   val ReleaseQueueHead = RegInit(UInt(8.W), 0.U)
 
   io.load_queue_index_out := LoadQueueIndexList(ReleaseQueueTail)
-  io.should_release_load_out := false.B
+  for (i <- 0 until coreWidth) io.should_release_load_out(i) := false.B
 
-  when(io.shadow_buffer_head_in > ShadowStampList(ReleaseQueueTail)) {
-    io.should_release_load_out := true.B
-    ReleaseQueueTail := (ReleaseQueueTail + 1.U) % 64.U
+  for (w <- 0 until coreWidth) {
+    when(io.shadow_buffer_head_in > ShadowStampList(ReleaseQueueTail)) {
+      io.should_release_load_out(w) := true.B
+      ReleaseQueueTail := (ReleaseQueueTail + 1.U) % 64.U
+    }
   }
 
-  when(io.new_entry_in) {
-    ShadowStampList(ReleaseQueueHead) := io.shadow_buffer_tail_in
-    LoadQueueIndexList(ReleaseQueueHead) := io.load_queue_index_in
-    ReleaseQueueHead := (ReleaseQueueHead + 1.U) % 64.U
+  for (w <- 0 until coreWidth) {
+    when(io.new_entry_in(w)) {
+      ShadowStampList(ReleaseQueueHead) := io.shadow_buffer_tail_in
+      LoadQueueIndexList(ReleaseQueueHead) := io.load_queue_index_in(w)
+      ReleaseQueueHead := (ReleaseQueueHead + 1.U) % 64.U
+    }
   }
-
 
 }
